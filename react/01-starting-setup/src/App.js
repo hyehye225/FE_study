7a943584f349771d72817245b9fabc6e7f1395fe
hyehye225 <h3192@naver.com>
2023-10-27 22:33:08 +0900

    과제 1: 연습해봅시다: React & 컴포넌트 기초

--- react/01-starting-setup/src/App.js
+++ react/01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expenses from "./components/Expenses";
+import "./components/Expenses.css";
 function App() {
   const expenses = [
     {
@@ -23,27 +23,30 @@ function App() {
     },
   ];
   return (
+    // <>
+    //   <ExpenseItem
+    //     title={expenses[0].title}
+    //     amount={expenses[0].amount}
+    //     date={expenses[0].date}
+    //   />
+    //   <ExpenseItem
+    //     title={expenses[1].title}
+    //     amount={expenses[1].amount}
+    //     date={expenses[1].date}
+    //   />
+    //   <ExpenseItem
+    //     title={expenses[2].title}
+    //     amount={expenses[2].amount}
+    //     date={expenses[2].date}
+    //   />
+    //   <ExpenseItem
+    //     title={expenses[3].title}
+    //     amount={expenses[3].amount}
+    //     date={expenses[3].date}
+    //   />
+    // </>
     <>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses} />
     </>
   );
 }
