f9e9f74b56aa13d5ecdfe4940bf525afc62a1bfd
hyehye225 <h3192@naver.com>
2023-10-27 21:42:18 +0900

    51. props를 전달하고 받는 대안적 방법/props 처리하기

--- react/01-starting-setup/src/components/ExpenseItem.js
+++ react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,22 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-  //   const expenseDate = new Date(2023, 10, 27);
-  //   const expenseTitle = "Car Insurance";
-  //   const expenseAmount = 294.67;
-  const month = props.date.toLocaleString("ko", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("ko", { day: "2-digit" });
-
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
