89e6a23118117636c1719d4f80d876e6c270e962
hyehye225 <h3192@naver.com>
2023-10-27 21:26:49 +0900

    toLocaleString() and getFullYear()

--- react/01-starting-setup/src/components/ExpenseItem.js
+++ react/01-starting-setup/src/components/ExpenseItem.js
@@ -3,15 +3,19 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2023, 10, 27);
   //   const expenseTitle = "Car Insurance";
   //   const expenseAmount = 294.67;
-  const expenseDate = props.date;
+  const month = props.date.toLocaleString("ko", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("ko", { day: "2-digit" });
+
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
       <div>
-        {expenseDate.getFullYear()}년 {expenseDate.getMonth()}월{" "}
-        {expenseDate.getDate()}일
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
