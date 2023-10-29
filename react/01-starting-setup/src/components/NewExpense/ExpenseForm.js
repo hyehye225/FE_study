1ddf34cd11e806c5ee2171e2cc654a21eca947c0
hyehye225 <h3192@naver.com>
2023-10-29 11:15:40 +0900

    79. 양방향 바인딩 추가하기

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,9 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
     console.log(newExpense);
   };
   return (
@@ -29,12 +31,17 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -44,6 +51,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2023-10-28"
             max="2023-12-31"
             onChange={dateChangeHandler}
