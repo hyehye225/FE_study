f315002325c9920cb1c9b4a512dee0b7fc171ac4
hyehye225 <h3192@naver.com>
2023-11-07 19:10:33 +0900

    181. 아이템을 이동식으로 만들기

--- react/section 12 project/src/components/Cart/Cart.js
+++ react/section 12 project/src/components/Cart/Cart.js
@@ -6,8 +6,12 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
