1ab33adb8fd7b19b820bab1c10025dc11f2977f2
hyehye225 <h3192@naver.com>
2023-11-12 17:51:02 +0900

    214. 요청에 useEffect() 사용하기

--- react/section 15 project/src/App.js
+++ react/section 15 project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,7 +7,8 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -31,8 +32,10 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
-
+  });
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
   return (
     <React.Fragment>
       <section>
