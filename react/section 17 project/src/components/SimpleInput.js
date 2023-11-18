1d2f055a8742a80455ce528870af4edd86c350b9
hyehye225 <h3192@naver.com>
2023-11-18 19:27:59 +0900

    243. 검증 피드백 제공하기

--- react/section 17 project/src/components/SimpleInput.js
+++ react/section 17 project/src/components/SimpleInput.js
@@ -2,22 +2,29 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
     console.log(enteredName);
-
+    setEnteredNameIsValid(true);
     setEnteredName("");
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
   };
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           value={enteredName}
@@ -26,6 +33,9 @@ const SimpleInput = (props) => {
           id="name"
           onChange={nameInputChangeHandler}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
