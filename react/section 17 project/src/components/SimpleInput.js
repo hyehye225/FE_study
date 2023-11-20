b43040a2542a6fafd229f226c146b85891e0b44a
hyehye225 <h3192@naver.com>
2023-11-20 19:37:15 +0900

    245. 초점을 잃은 리액트

--- react/section 17 project/src/components/SimpleInput.js
+++ react/section 17 project/src/components/SimpleInput.js
@@ -13,6 +13,14 @@ const SimpleInput = (props) => {
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -42,6 +50,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
         />
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
