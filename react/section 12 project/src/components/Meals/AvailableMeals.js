fe237d6079e36f42a1fcd1261de1e56f5addd135
hyehye225 <h3192@naver.com>
2023-11-25 13:51:20 +0900

    260. 로딩 State 다루기

--- react/section 12 project/src/components/Meals/AvailableMeals.js
+++ react/section 12 project/src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -22,9 +23,17 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
