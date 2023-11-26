a60455893982614e8d86cbd9aa8d57596e4f6828
hyehye225 <h3192@naver.com>
2023-11-26 14:25:35 +0900

    275. 더 많은 리덕스 기본 사항

--- react/section 19/redux-demo.js
+++ react/section 19/redux-demo.js
@@ -2,9 +2,17 @@ const redux = require("redux");
 
 // 최초로 실행될 때, default value가 존재해야 한다.
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 const store = redux.createStore(counterReducer);
 
@@ -18,3 +26,4 @@ store.subscribe(counterSubscriber);
 store.dispatch({
   type: "increment",
 }); // dispatch는 액션을 발송하는 메소드
+store.dispatch({ type: "decrement" });
