8b251ae12ad9947bafcc17b04504f86b28a42857
hyehye225 <h3192@naver.com>
2023-11-26 14:57:54 +0900

    278. 스토어 제공하기

--- react/section 19 project/src/store/index.js
+++ react/section 19 project/src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type == "increment") {
+  if (action.type === "increment") {
     return {
       state: state.counter + 1,
     };
   }
-  if (action.type == "decrement") {
+  if (action.type === "decrement") {
     return {
       state: state.counter - 1,
     };
