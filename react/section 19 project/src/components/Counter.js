6605e433f7c90cbf79822d7e670d16f3b3da86ee
hyehye225 <h3192@naver.com>
2023-12-04 13:17:43 +0900

    281. 클래스 기반 컴포넌트가 있는 리덕스

--- react/section 19 project/src/components/Counter.js
+++ react/section 19 project/src/components/Counter.js
@@ -25,28 +25,39 @@ const Counter = () => {
     </main>
   );
 };
-class Counter extends Component {
-  incrementHandler() {}
-  decrementHandler() {}
-  toggleCounterHandler() {}
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div>
-          <button onClick={this.incrementHandler}>Increment</button>
-          <button onClick={this.decrementHandler}>Decrement</button>
-        </div>
-        <div className={classes.value}>{counter}</div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-export default connect()(Counter);
+export default Counter;
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+//   toggleCounterHandler() {}
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
