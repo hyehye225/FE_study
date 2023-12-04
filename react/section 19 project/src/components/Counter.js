7ddb232b746d66f7adcddad26ad2869e84828d16
hyehye225 <h3192@naver.com>
2023-12-04 11:17:20 +0900

    bare minimum 풀이 완료

--- react/section 19 project/src/components/Counter.js
+++ react/section 19 project/src/components/Counter.js
@@ -1,5 +1,6 @@
 import classes from "./Counter.module.css";
-import { useSelector, useDispatch } from "react-redux";
+import { Component } from "react";
+import { useSelector, useDispatch, connect } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
@@ -24,5 +25,28 @@ const Counter = () => {
     </main>
   );
 };
+class Counter extends Component {
+  incrementHandler() {}
+  decrementHandler() {}
+  toggleCounterHandler() {}
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div>
+          <button onClick={this.incrementHandler}>Increment</button>
+          <button onClick={this.decrementHandler}>Decrement</button>
+        </div>
+        <div className={classes.value}>{counter}</div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+export default connect()(Counter);
