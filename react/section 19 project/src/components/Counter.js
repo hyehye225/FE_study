94895181509b65b9c2120554fccb92bc71d2389d
hyehye225 <h3192@naver.com>
2023-12-04 13:31:21 +0900

    282. 작업에 페이로드 연결하기

--- react/section 19 project/src/components/Counter.js
+++ react/section 19 project/src/components/Counter.js
@@ -10,6 +10,9 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -18,6 +21,7 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <div className={classes.value}>{counter}</div>
