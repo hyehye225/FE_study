73faebbdc87b3262cd6a2387c0c62eae447e580c
hyehye225 <h3192@naver.com>
2023-12-10 21:19:07 +0900

    291. 코드 분할하기

--- react/section 19 project/src/components/Counter.js
+++ react/section 19 project/src/components/Counter.js
@@ -1,11 +1,11 @@
 import classes from "./Counter.module.css";
 import { Component } from "react";
-import { counterActions } from "../store/index";
+import { counterActions } from "../store/counter";
 import { useSelector, useDispatch, connect } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
   const toggleCounterHandler = () => {
     dispatch(counterActions.toggleCounter());
   };
