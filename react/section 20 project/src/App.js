6f698edfda546b9d2abab7388b674e83120d8621
hyehye225 <h3192@naver.com>
2023-12-17 13:35:31 +0900

    304. 액션 생성자 Thunk 사용하기

--- react/section 20 project/src/App.js
+++ react/section 20 project/src/App.js
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 let isInitial = true;
 function App() {
   const dispatch = useDispatch();
@@ -12,53 +12,11 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-6485e-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        dispatch(
-          uiActions.showNotification({
-            status: "Error",
-            title: "Error!",
-            message: "Sent cart data failed!",
-          })
-        );
-      }
-      const responseData = await response.json();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success...",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sent cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <>
