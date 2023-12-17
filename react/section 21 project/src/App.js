bcb395d0ce3a741e924093eb37d9064484d30931
hyehye225 <h3192@naver.com>
2023-12-17 17:32:41 +0900

    314. 두 번째 라우트 추가하기

--- react/section 21 project/src/App.js
+++ react/section 21 project/src/App.js
@@ -1,6 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import ProductsPage from "./pages/Product";
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
