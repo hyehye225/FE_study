dddc1f816dcc5358024565956c0f6886fe13ead8
hyehye225 <h3192@naver.com>
2023-12-17 15:04:43 +0900

    306. 가져오기 로직 마무리하기

--- react/section 20 project/src/store/cart-actions.js
+++ react/section 20 project/src/store/cart-actions.js
@@ -15,7 +15,12 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -41,7 +46,10 @@ export const sendCartData = (cart) => {
         "https://react-http-6485e-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
